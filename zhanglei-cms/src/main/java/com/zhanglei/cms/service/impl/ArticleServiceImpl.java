a4abf636290438969e032b4583bfe7126fe20023
zhanglei-warehouse <zl9.5@qq.com>
2019-12-19 13:26:17 +0800

    上传项目

--- zhanglei-cms/src/main/java/com/zhanglei/cms/service/impl/ArticleServiceImpl.java
+++ zhanglei-cms/src/main/java/com/zhanglei/cms/service/impl/ArticleServiceImpl.java
@@ -105,7 +105,7 @@ public class ArticleServiceImpl implements ArticleService {
 
 	@Override
 	public PageInfo<Article> getListByChannelIdAndCateId(Integer channelId, Integer cateId, Integer pageNum) {
-		PageHelper.startPage(pageNum, 1);
+		PageHelper.startPage(pageNum, 3);
 		List<Article> articleList = articleDao.selectListByChannelIdAndCateId(channelId,cateId);
 		 return new PageInfo<>(articleList);
 	}
