4d35f0684f6897a410090e3c863b8ad173101444
zhanglei-warehouse <zl9.5@qq.com>
2019-12-24 08:17:32 +0800

    添加springbeanutil工具类

---
+++ zhanglei-cms/src/main/java/com/zhanglei/cms/common/SpringBeanUtils.java
@@ -0,0 +1,32 @@
+package com.zhanglei.cms.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringBeanUtils implements ApplicationContextAware{
+	
+	private static ApplicationContext applicationContext = null;
+	
+	@Autowired
+	public void setApplicationContext(ApplicationContext applicationContext) {
+		if(SpringBeanUtils.applicationContext == null) {
+			SpringBeanUtils.applicationContext = applicationContext;
+		}
+	}
+
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
+	
+	public static <T> T getBean(String name,Class<T> clazz) {
+		return applicationContext.getBean(name, clazz);
+	}
+	
+	public static <T> T getBean(Class<T> clazz) {
+		return applicationContext.getBean(clazz);
+	}
+
+}
